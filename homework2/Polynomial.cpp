5b9c58d0ec88117ddc031aacc97852804f2c233e
vaskers5 <danil.krokodil@mail.ru>
2021-03-30 00:20:14 +0300

    fixed to do and add cpp

--- homework2/Polynomial.cpp
+++ homework2/Polynomial.cpp
@@ -1,62 +1,228 @@
-#include <iostream>
-#include "Polynomial.h"
-#include <sstream>
-#include <assert.h>
-
+#include "polynomial.h"
 
 using namespace std;
 
-void checkOut() {
-    stringstream ss;
-    ss << Polynomial(0, 2, new int[3]{ 1, 2, 3 }) << " ";
-    ss << Polynomial(1, 2, new int[2]{ -3, 4 }) << " ";
-    ss << Polynomial(-1, 2, new int[4]{ 1, -3, 0, -1 }) << " ";
-    ss << Polynomial() << " ";
-    ss << Polynomial(0, 2, new int[3]{ 0, 3, -3 }) << " ";
-    ss << Polynomial(-2, 2, new int[5]{ 1, 0, 0, 0, 1 }) << " ";
-    assert(ss.str() == "3x^2+2x+1 4x^2-3x -x^2-3+x^-1 0 -3x^2+3x x^2+x^-2 ");
+Polynomial::Polynomial() {
+    max_pow = 0;
+    min_pow = 0;
+    size = max_pow + min_pow + 1;
+    polynom = new int[size];
+    for (int i = 0; i < size; i++) {
+        polynom[i] = 0;
+    }
 }
 
-void PolynomialOperators() {
-    stringstream ss;
-    const auto a = Polynomial(0, 2, new int[3]{ 1, 2, 3 });
-    const auto b = Polynomial(1, 2, new int[2]{ -3, 4 });
-    auto c = Polynomial(-1, 2, new int[4]{ 1, -3, 0, -1 });
-    const auto d = Polynomial();
-    const auto e = Polynomial(0, 2, new int[3]{ 0, 3, -3 });
-    const auto f = Polynomial(-2, 2, new int[5]{ 1, 0, 0, 0, 1 });
-   // assert(a * b == b * a);
-    //assert(d * e == d);
-    //assert(a * b * c == a * (b * c));
-    //assert(a * b == Polynomial(1, 4, new int[4]{ -3, -2, -1, 12 }));
-    assert(a + b == b + a);
-    assert(d + e == e);
-    assert(a + b + c == a + (b + c));
-   // assert(5 * a == a * 5);
-    assert(a + b + c == Polynomial(-1, 2, new int[4]{ 1, -2, -1, 6 }));
-   // cout << c / 2<<"\n";
-  //  cout << Polynomial(0, 0, new int[1]{ -1 });
-  // assert(c / 2 == Polynomial(0, 0, new int[1]{ -1 })); 
-  //  assert(c.get(4) == -18.75); //аля 4*coef
-    assert(a[1] == 2);
-    assert(a[10] == 0);
-    c[5] = 3;
-  //  cout << c.get(4);
-   // assert(c.get(4) == 3053.25);
-    cout << c<<"\n";
-    cout << (-(-(-c)))<<"\n";
-    assert((-c) == (-(-(-c)))); 
-    auto h = -c;
-    assert(c[5] == 3 && h[5] == -3);
-    assert((-c)[5] == -3);
-    auto g = b;
-    g[3] = 0;
-    assert(g == b);
+Polynomial::Polynomial(int min, int max, const int *poly) {
+    max_pow = max;
+    min_pow = min;
+    size = max_pow - min_pow + 1;
+    polynom = new int[size];
+    for (int i = 0; i < size; i++) {
+        polynom[i] = poly[i];
+    }
+}
+//fixed delete[]
+Polynomial::~Polynomial() {
+    delete[] polynom;
 }
 
+Polynomial::Polynomial(const Polynomial &another) {
+    min_pow = another.min_pow;
+    max_pow = another.max_pow;
+    size = another.size;
+    polynom = new int[size];
+    for (int i = 0; i < size; i++) {
+        polynom[i] = another.polynom[i];
+    }
+}
 
-int main() {
-    checkOut();
-    PolynomialOperators();
-    return 0;
+//fixed no default
+Polynomial &Polynomial::operator=(const Polynomial &another) {
+    min_pow = another.min_pow;
+    max_pow = another.max_pow;
+    size = another.size;
+    polynom = new int[size];
+    for (int i = 0; i < size; i++) {
+        polynom[i] = another.polynom[i];
+    }
+    return *this;
+}
+
+int Polynomial::operator[](int number) const {
+    if (number < min_pow || number > max_pow)
+        return 0;
+    return polynom[number - min_pow];
+}
+
+int &Polynomial::operator[](int number) {
+    if (number < min_pow || number > max_pow)
+        make(min(min_pow, number), max(max_pow, number));
+    return polynom[number - min_pow];
+}
+
+void Polynomial::make(int new_min_pow, int new_max_pow) {
+    int new_size = new_max_pow - new_min_pow + 1;
+    int *kost = new int[new_size];
+    for (int i = 0; i < new_size; i++) {
+        kost[i] = 0;
+    }
+    Polynomial another(new_min_pow, new_max_pow, kost);
+    for (int it = max(min_pow, new_min_pow); it <= min(max_pow, new_max_pow); it++) {
+        another.polynom[it - new_min_pow] = polynom[it - min_pow];
+    }
+    *this = another;
+}
+
+bool Polynomial::operator==(const Polynomial &second) const {
+    stringstream s1, s2;
+    s1 << *this;
+    s2 << second;
+    return (s1.str() == s2.str());
+}
+
+bool Polynomial::operator!=(const Polynomial &second) const {
+    return !(*this == second);
+}
+
+//fixed from +=
+Polynomial Polynomial::operator+(const Polynomial &second) const {
+    Polynomial another = Polynomial();
+    another += *this;
+    another += second;
+
+    return another;
+
+}
+
+Polynomial &Polynomial::operator+=(const Polynomial &second) {
+    int new_min_pow = min(min_pow, second.min_pow);
+    int new_max_pow = max(max_pow, second.max_pow);
+    int new_size;
+    new_size = new_max_pow - new_min_pow + 1;
+
+    int *kost = new int[new_size];
+
+    for (int i = 0; i < new_max_pow - new_min_pow + 1; i++)
+        kost[i] = 0;
+
+    for (int i = 0; i < size; i++)
+        kost[min_pow + i - new_min_pow] += polynom[i];
+
+    for (int i = 0; i < second.size; i++)
+        kost[second.min_pow + i - new_min_pow] += second.polynom[i];
+
+    make(new_min_pow, new_max_pow);
+    for (int i = 0; i < size; i++)
+        this->polynom[i] = kost[i];
+    this->min_pow = new_min_pow;
+    this->max_pow = new_max_pow;
+    this->size = size;
+    return *this;
+}
+
+Polynomial Polynomial::operator-() const {
+    int *temp = new int[size];
+    for (int i = 0; i < size; i++)
+        temp[i] = -polynom[i];
+
+    return Polynomial(min_pow, max_pow, temp);
+}
+
+Polynomial Polynomial::operator-(const Polynomial &second) const {
+    Polynomial check = *this + (-second);
+    return check;
+}
+
+Polynomial & Polynomial::operator-=(const Polynomial &second) {
+    *this = (*this - second);
+    return *this;
+}
+
+Polynomial operator*(int number, Polynomial D) {
+    for (int i = 0; i < D.size; i++)
+        D.polynom[i] *= number;
+
+    return D;
+}
+
+Polynomial Polynomial::operator*=(int number) {
+    return *(this) * number;
+}
+
+Polynomial operator*(const Polynomial &first, const Polynomial &second) {
+    int new_min = first.min_pow * second.min_pow;
+    int new_max = first.max_pow * second.max_pow;
+    int new_size = new_max - new_min + 1;
+    int *comb_pol = new int[new_size];
+
+    for (int i = 0; i < new_size; i++)
+        comb_pol[i] = 0;
+    for (int i = 0; i < first.size; i++)
+        for (int j = 0; j < second.size; j++)
+            comb_pol[first.min_pow + i + second.min_pow + j - new_min] += first.polynom[i] * second.polynom[j];
+    return Polynomial(new_min, new_max, comb_pol);
+}
+
+Polynomial operator/(const Polynomial &another, int number) {
+    int *kost = new int[another.size];
+    //todo for_each
+    for (int i = 0; i < another.size; i++)
+        kost[i] = another.polynom[i] / number;
+
+    return Polynomial(another.min_pow, another.max_pow, kost);
+}
+
+Polynomial Polynomial::operator/=(int number) const {
+    return *this / number;
+}
+
+//fixed get O(n)
+double Polynomial::get(int number) {
+    double it = polynom[0] * powf(number, min_pow);
+    double ans = it;
+    for (int i = 1; i < size; i++) {
+        it *= number;
+        ans += polynom[i] * it;
+    }
+    return ans;
+}
+
+ostream &operator<<(ostream &output, const Polynomial &D) {
+    if (D.size == 1) {
+        output << D.polynom[0];
+        return output;
+    } else {
+        int it = D.max_pow;
+        int counter = 0;
+        for (int i = D.size - 1; i >= 0; i--) {
+            if (D.polynom[i] != 0) {
+                if (counter > 0 and D.polynom[i] > 0)
+                    output << "+";
+
+                counter++;
+
+                if (it != 1 and it != 0 and abs(D.polynom[i]) != 1)
+                    output << D.polynom[i] << "x^" << it;
+
+                if (it != 1 and it != 0 and D.polynom[i] == -1)
+                    output << "-" << "x^" << it;
+
+                if (it != 1 and it != 0 and D.polynom[i] == 1)
+                    output << "x^" << it;
+
+                if (it == 1 and abs(D.polynom[i]) != 1)
+                    output << D.polynom[i] << "x";
+
+                if (it == 1 and D.polynom[i] == 1)
+                    output << "x";
+
+                if (it == 1 and D.polynom[i] == -1)
+                    output << "-x";
+                if (it == 0)
+                    output << D.polynom[i];
+            }
+            it--;
+        }
+        return output;
+    }
 }
--- homework2/Polynomial.h
+++ homework2/Polynomial.h
@@ -1,285 +1,70 @@
 #pragma once
+
 #include <iostream>
 #include <vector>
 #include <cmath>
 #include <sstream>
 
-using  namespace std;
+using namespace std;
 
-class Polynomial
-{
+class Polynomial {
 private:
     int size, max_pow, min_pow;
-    int* polynom;
+    int *polynom;
 
 public:
 
-    Polynomial()
-    {
-        max_pow = 0;
-        min_pow = 0;
-        size = max_pow + min_pow + 1;
-        polynom = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            polynom[i] = 0;
-        }
-    }
+    Polynomial();
 
-    Polynomial(int min, int max, int* poly)
-    {
-        max_pow = max;
-        min_pow = min;
-        size = max_pow - min_pow + 1;
-        polynom = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            polynom[i] = poly[i];
-        }
-    }
+    Polynomial(int min, int max, const int *poly);
 
     //fixed delete[]
-    ~Polynomial()
-    {
-        delete[] polynom;
-    }
-    //fixed no default
-    Polynomial(const Polynomial& another)
-    {
-        min_pow = another.min_pow;
-        max_pow = another.max_pow;
-        size = another.size;
-        polynom = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            polynom[i] = another.polynom[i];
-        }
-        // return *this;
-    }
+    ~Polynomial();
 
     //fixed no default
-    Polynomial& operator=(const Polynomial& another)
-    {
-        min_pow = another.min_pow;
-        max_pow = another.max_pow;
-        size = another.size;
-        polynom = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            polynom[i] = another.polynom[i];
-        }
-        return *this;
+    Polynomial(const Polynomial &another);
+
+    //fixed no default
+    Polynomial &operator=(const Polynomial &another);
+
+    int operator[](int number) const;
+
+    int &operator[](int number);
+
+    void make(int new_min_pow, int new_max_pow);
+
+    bool operator==(const Polynomial &second) const;
+
+    bool operator!=(const Polynomial &second) const;
+
+    //fixed from +=
+    Polynomial operator+(const Polynomial &second) const;
+
+    Polynomial &operator+=(const Polynomial &second);
+
+    Polynomial operator-() const;
+
+    Polynomial operator-(const Polynomial &second) const;
+
+
+    Polynomial &operator-=(const Polynomial &second);
+
+    friend Polynomial operator*(int number, Polynomial D);
+
+    friend Polynomial operator*(const Polynomial &D, int number) {
+        return number * D;
     }
 
-    Polynomial (int min, int max)
-    {
-        max_pow = max;
-        min_pow = min;
-        size = max_pow - min_pow + 1;
-        polynom = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            polynom[i] = 0;
-        }
-    }
-    int operator[](int number) const
-    {
-        if (number < min_pow || number > max_pow)
-            return 0;
-        return polynom[number - min_pow];
-    }
+    Polynomial operator*=(int number);
 
-    int& operator[](int number)
-    {
-        if (number < min_pow || number > max_pow)
-            make(min(min_pow, number), max(max_pow, number));
-        return polynom[number - min_pow];
-    }
+    friend Polynomial operator*(const Polynomial &first, const Polynomial &second);
 
-    void make(int new_min_pow, int new_max_pow)
-    {
-        int  new_size =  new_max_pow-new_min_pow + 1;
-        int* kost = new int[new_size];
-        for (int i = 0; i < new_size; i++)
-        {
-            kost[i] = 0;
-        }
-        Polynomial another(new_min_pow, new_max_pow,kost);
-        for (int it = max(min_pow, new_min_pow); it <= min(max_pow, new_max_pow); it++)
-        {
-            another.polynom[it - new_min_pow] = polynom[it - min_pow];
-        }
-        *this = another;
-    }
+    friend Polynomial operator/(const Polynomial &another, int number);
 
-    bool operator==(const Polynomial& second) const
-    {
-        stringstream s1,s2;
-        s1<<*this;
-        s2<<second;
-        return (s1.str()==s2.str());
-    }
+    Polynomial operator/=(int number) const;
 
-    bool operator!=(const Polynomial& second)
-    {
-        return !(*this == second);
-    }
+    //fixed get O(n)
+    double get(int number);
 
-    //todo + from +=
-    Polynomial operator+(const Polynomial& second) const
-    {
-        int new_min_pow = min(min_pow, second.min_pow);
-        int new_max_pow = max(max_pow, second.max_pow);
-        int new_size;
-        new_size = new_max_pow - new_min_pow + 1;
-
-        int* kost = new int[new_size];
-
-        for (int i = 0; i < new_max_pow - new_min_pow + 1; i++)
-            kost[i] = 0;
-
-        for (int i = 0; i < size; i++)
-            kost[min_pow + i - new_min_pow] += polynom[i];
-
-        for (int i = 0; i < second.size; i++)
-            kost[second.min_pow + i - new_min_pow] += second.polynom[i];
-
-        return Polynomial(new_min_pow, new_max_pow, kost);
-
-    }
-
-    Polynomial& operator +=(const Polynomial& second)
-    {
-        *this = (*this + second);
-        return *this;
-    }
-
-    Polynomial operator-() const
-    {
-        int *temp = new int[size];
-        for (int i = 0; i < size; i++)
-            temp[i] = -polynom[i];
-
-        return Polynomial(min_pow, max_pow, temp);
-    }
-
-    Polynomial operator-(const Polynomial& second)
-    {
-        Polynomial check = *this + (-second);
-        return check;
-    }
-
-
-    Polynomial& operator -=(const Polynomial& second)
-    {
-        *this = (*this - second);
-        return *this;
-    }
-    //to do with copy-constructor
-    friend Polynomial operator*(int number,Polynomial D)
-    {
-        for (int i = 0; i < D.size; i++)
-            D.polynom[i] *= number;
-
-        return D;
-    }
-
-    friend Polynomial operator*(Polynomial D,int number)
-    {
-        return number*D;
-    }
-
-    Polynomial operator *=(int number)
-    {
-        return *(this) * number;
-    }
-
-    friend Polynomial operator*(const Polynomial& first, const Polynomial& second)
-    {
-        int new_min = first.min_pow*second.min_pow;
-        int new_max = first.max_pow*second.max_pow;
-        int new_size = new_max - new_min + 1;
-        int* comb_pol = new int[new_size];
-
-        for (int i = 0; i < new_size; i++)
-            comb_pol[i] = 0;
-        for (int i = 0; i < first.size; i++)
-            for (int j = 0; j < second.size; j++)
-                comb_pol[first.min_pow + i + second.min_pow + j - new_min] += first.polynom[i] * second.polynom[j];
-        return Polynomial(new_min, new_max, comb_pol);
-    }
-
-    friend Polynomial operator/(const Polynomial& another, int number)
-    {
-        int* kost = new int[another.size];
-        //todo for_each
-        for (int i = 0; i < another.size; i++)
-            kost[i] = another.polynom[i] / number;
-
-        return Polynomial(another.min_pow,another.max_pow,kost);
-    }
-
-    Polynomial operator /=(int number)
-    {
-        return *this / number;
-    }
-
-    //todo get O(n)
-    double get(int number)
-    {
-        double it = polynom[0]*powf(number,min_pow);
-        double ans = it;
-        for (int i = 1; i < size; i++)
-        {
-            it*=number;
-            ans += polynom[i]*it;
-        }
-        return ans;
-    }
-
-    friend ostream& operator<<(ostream& output, const Polynomial& D)
-    {
-        if (D.size == 1)
-        {
-            output << D.polynom[0];
-            return output;
-        }
-        else
-        {
-            int it = D.max_pow;
-            int counter = 0;
-            for (int i = D.size - 1; i >= 0; i--)
-            {
-                if (D.polynom[i] != 0)
-                {
-                    if (counter > 0 and D.polynom[i] > 0)
-                        output << "+";
-
-                    counter++;
-
-                    if (it != 1 and it != 0 and abs(D.polynom[i]) != 1)
-                        output << D.polynom[i] << "x^" << it;
-
-                    if (it != 1 and it != 0 and D.polynom[i] == -1)
-                        output << "-" << "x^" << it;
-
-                    if (it != 1 and it != 0 and D.polynom[i] == 1)
-                        output << "x^" << it;
-
-                    if (it == 1 and abs(D.polynom[i])!=1)
-                        output << D.polynom[i] << "x";
-
-                    if (it == 1 and D.polynom[i]==1)
-                        output << "x";
-
-                    if (it == 1 and D.polynom[i]==-1)
-                        output << "-x";
-                    if (it == 0)
-                        output << D.polynom[i];
-                }
-                it--;
-            }
-            return output;
-        }
-    }
+    friend ostream &operator<<(ostream &output, const Polynomial &D);
 };
