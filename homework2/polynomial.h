7349d99e8826744627e3a0a34b1c67ed9790a6f0
Kazan_Plova <48238046+vaskers5@users.noreply.github.com>
2021-03-30 13:53:01 +0300

    Update polynomial.h

--- homework2/polynomial.h
+++ homework2/polynomial.h
@@ -1,6 +1,5 @@
-<<<<<<< HEAD:homework2/Polynomial.h
-#pragma once
 
+#pragma once
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -69,75 +68,4 @@ public:
     double get(int number);
 
     friend ostream &operator<<(ostream &output, const Polynomial &D);
-=======
-#pragma once
-
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <sstream>
-
-using namespace std;
-
-class Polynomial {
-private:
-    int size, max_pow, min_pow;
-    int *polynom;
-
-public:
-
-    Polynomial();
-
-    Polynomial(int min, int max, const int *poly);
-
-    //fixed delete[]
-    ~Polynomial();
-
-    //fixed no default
-    Polynomial(const Polynomial &another);
-
-    //fixed no default
-    Polynomial &operator=(const Polynomial &another);
-
-    int operator[](int number) const;
-
-    int &operator[](int number);
-
-    void make(int new_min_pow, int new_max_pow);
-
-    bool operator==(const Polynomial &second) const;
-
-    bool operator!=(const Polynomial &second) const;
-
-    //fixed from +=
-    Polynomial operator+(const Polynomial &second) const;
-
-    Polynomial &operator+=(const Polynomial &second);
-
-    Polynomial operator-() const;
-
-    Polynomial operator-(const Polynomial &second) const;
-
-
-    Polynomial &operator-=(const Polynomial &second);
-
-    friend Polynomial operator*(int number, Polynomial D);
-
-    friend Polynomial operator*(const Polynomial &D, int number) {
-        return number * D;
-    }
-
-    Polynomial operator*=(int number);
-
-    friend Polynomial operator*(const Polynomial &first, const Polynomial &second);
-
-    friend Polynomial operator/(const Polynomial &another, int number);
-
-    Polynomial operator/=(int number) const;
-
-    //fixed get O(n)
-    double get(int number);
-
-    friend ostream &operator<<(ostream &output, const Polynomial &D);
->>>>>>> 77f42ef8aa0a3e9a6dd671a13f0bc3901f7c388c:homework2/polynomial.h
 };
