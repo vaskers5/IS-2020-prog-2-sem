91ec61575971bcf31e1cfce9f63ab80334ec7ca4
vaskers5 <danil.krokodil@mail.ru>
2021-05-18 13:07:11 +0300

    add smt

---
+++ homework5/cmake-build-debug/CMakeFiles/homework5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework5.dir/main.cpp.obj"
+  "homework5.exe"
+  "homework5.exe.manifest"
+  "homework5.pdb"
+  "libhomework5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
